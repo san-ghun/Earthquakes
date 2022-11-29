61063db5d0ac6c7bc57e5852d069ef9dab232f96
Sanghun <po4865@gmail.com>
2022-11-29 23:05:18 +0100

    feat: Test the decoder
    Add tests and sample test data to verify that the app reliably decodes a GeoJSON feature.
    
    - Add a new test target to the project to create a testing environment, by adding Unit Testing Bundle.
    
    TestData.swift
    - Create new Swift file to test group.
    - Create a data constant by encoding a string literal in GeoJSON format, using triple-quote syntax.
    
    EarthquakesTests.swift
    - Comment the test functions that Xcode provided.
    - Use the @testable attribute to give the unit tests access to the app's internal functions and types.
    - 14: Create a test case `testGeoJSONDecodesQuake()` that throws an error.
    - 15,17-19: Try to decode the GeoJSON data. asserting that the code property in the Quake instance matches the corresponding value from the sample data.
    - 21-22: Create constants to compare the expected time from the sample data to the decoded quake time.
    - 24: Add another assertion to validate the time of the quake.
    - 16: Update the JSON decoder to decode time as milliseconds.

---
+++ EarthquakesTests/EarthquakesTests.swift
@@ -0,0 +1,53 @@
+//
+//  EarthquakesTests.swift
+//  EarthquakesTests
+//
+//  Created by Sanghun Park on 29.11.22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import XCTest
+@testable import Earthquakes
+
+class EarthquakesTests: XCTestCase {
+    
+    func testGeoJSONDecoderDecodesQuake() throws {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .millisecondsSince1970
+        let quake = try decoder.decode(Quake.self, from: testFeature_nc73649170)
+        
+        XCTAssertEqual(quake.code, "73649170")
+        
+        let expectedSeconds = TimeInterval(1636129710550) / 1000
+        let decodedSeconds = quake.time.timeIntervalSince1970
+        
+        XCTAssertEqual(expectedSeconds, decodedSeconds, accuracy: 0.00001)
+    }
+}
+
+/*final class EarthquakesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}*/
